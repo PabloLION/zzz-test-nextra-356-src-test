f9132952e89f38c36c04614ce0d3934972d5b5dc
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-07-28 02:12:11 +0200

    feat: test nextra-theme-blog@2.0.0-beta.10 , #597

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 };
 
 const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
+  theme: "nextra-theme-blog",
   themeConfig: "./theme.config.js",
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
